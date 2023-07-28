057128fec696a92db838addce04f436c70c40aa6
Adrian Legion <nlawliet6@gmail.com>
2023-07-28 01:02:22 -0400

    dns resolution

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {  // they are equivalent, function can be hoisted(declared after
 
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              DNS unresolved: <code>erc.adriannyc.dev</code> to url<code>adriannyc.dev/erc20-faucet</code>
+              DNS unresolved: <code>erc.adriannyc.dev</code> to url<code>adriannyc.dev/erc20-faucetv2</code>
             </p>
             <a
               className="App-link"
